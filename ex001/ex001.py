2ec75103f2ffafb9a3ffb4a857215a6309020327
Erasmo <erasmojunior456@gmail.com>
2026-06-01 10:55:54 -0300

    declarações

---
+++ ex001/ex001.py
@@ -0,0 +1,19 @@
+# Declaração de Classe
+class Gafanhoto:
+    def __init__(self): # Método Construtor
+        # Atributos de Instância
+        self.nome = ""
+        self.idade = 0
+
+    # Métodos de Intância
+    def aniversario(self):
+        self.idade += 1
+
+    def mensagem(self):
+        return f"{self.nome} é Gafanhoto(a) e tem {self.idade} anos de idade."
+
+# Declaração de Objetos
+g1 = Gafanhoto()
+g1.nome = input("QUal nome do Gafanhoto? ")
+g1.idade = int(input("Qual idade do Gafanhoto? "))
+print(g1.mensagem())
