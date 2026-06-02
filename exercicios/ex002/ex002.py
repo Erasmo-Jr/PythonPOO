ea27d2ebb62ac210e79bdb9d1859d69c138615ac
Erasmo <erasmojunior456@gmail.com>
2026-06-02 16:52:12 -0300

    def __getstate__

--- exercicios/ex002/ex002.py
+++ exercicios/ex002/ex002.py
@@ -16,19 +16,25 @@ class Gafanhoto:
     def aniversario(self):
         self.idade += 1
 
-    def mensagem(self):
+
+    def __str__(self): # Dunder Method
         return f"{self.nome} é Gafanhoto(a) e tem {self.idade} anos de idade."
+
+    def __getstate__(self):
+        return f"Estado: nome = {self.nome}, idade = {self.idade}"
 
 # Declaração de Objetos
 g1 = Gafanhoto("Maria", 17)
 g1.aniversario()
-print(g1.mensagem())
+print(g1)
+print(g1.__dict__) # Attribute
+print(g1.__getstate__()) # Method
 
 g2 = Gafanhoto("Mauro", 53)
 g2.aniversario()
-print(g2.mensagem())
+print(g2)
 
 g3 = Gafanhoto()
-print(g3.mensagem())
+print(g3)
 
 print(g1.__doc__) # Dunder Attribute
