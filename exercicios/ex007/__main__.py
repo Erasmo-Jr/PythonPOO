36e5f7abdb37998f2af340d345234a10715cea09
Erasmo <erasmojunior456@gmail.com>
2026-06-26 20:21:04 -0300

    ex007

---
+++ exercicios/ex007/__main__.py
@@ -0,0 +1,22 @@
+from rich import print, inspect
+from classe import Aluno, Professor, Funcionario, Pessoa
+
+a1 = Aluno('Erasmo', 25, 'Engenharia de Software', 'T01')
+a1.fazer_aniversario()
+a1.fazer_matricula()
+#inspect(a1, methods=True)
+
+p1 = Professor('Guanabara', 37, 'Biologia', 'Mestre')
+p1.fazer_aniversario()
+p1.dar_aula()
+#inspect(p1, methods=True)
+
+f1 = Funcionario('Cláudia', 27, 'Secretaria Escolar', 'Secretaria')
+f1.fazer_aniversario()
+f1.bater_ponto()
+#inspect(f1, methods=True)
+
+a1.estudar()
+p1.estudar()
+f1.estudar()
+
