5ac82c8465ec6ec291661ddc8cfd440ea5616ba3
Erasmo <erasmojunior456@gmail.com>
2026-07-20 09:17:33 -0300

    encapsulamento na pratica

--- exercicios/ex008/__main__.py
+++ exercicios/ex008/__main__.py
@@ -2,8 +2,13 @@ from ex008 import ContaBancaria
 
 def main():
     c1 = ContaBancaria(111, "Maria", 5_000)
-    c1.depositar(500)
-    c1.sacar(100)
+    c1.depositar(1000)
+    
+    c1.titular = "Pedro" # Aqui ele cria outro e deixa o original intacto
+    c1._titular = "Pedro" # Ele deixa, mas não mexa pois 'Adultos estão consentindo'...
+
+    c1.__saldo = 0 # Aqui no privado ele não deixa criar
+    c1._ContaBancaria__saldo = 0 # Aqui já muda
 
     print(c1)
 
--- exercicios/ex008/ex008.py
+++ exercicios/ex008/ex008.py
@@ -8,25 +8,27 @@ class ContaBancaria:
     Cria uma conta bancária e permite fazer saques e depósitos
     """
     def __init__(self, id, nome, saldo = 0):
-        self.id = id
-        self.titular = nome
-        self.saldo = saldo
-        print(f'A conta {self.id} de {self.titular} tem R${self.saldo:,.2f} de saldo')
+        self.id = id # público (+)
+        self._titular = nome # protegido (#)
+        self.__saldo = saldo # privado (-)
+        print(f'A conta {self.id} de {self._titular} tem R${self.__saldo:,.2f} de __saldo')
 
     def __str__(self):
-        #return f'A conta {self.id} de {self.titular} tem R${self.saldo:,.2f} de saldo'
+        #return f'A conta {self.id} de {self._titular} tem R${self.__saldo:,.2f} de __saldo'
         return f'Estudo atual da conta {self.__dict__}'
 
     def depositar(self, valor):
-        self.saldo += valor
-        print(f"Deposito: {self.titular} de {valor:,.2f} reais na conta {self.id}")
+        valor = abs(valor)
+        self.__saldo += valor
+        print(f"Deposito: {self._titular} de {valor:,.2f} reais na conta {self.id}")
 
     def sacar(self, valor):
-        if valor >= self.saldo:
+        valor = abs(valor)
+        if valor >= self.__saldo:
             print(f"Saque NEGADO de R$ {valor:,.2f} na conta {self.id}: SALDO INSUFICIENTE")
         else:
-            self.saldo -= valor
-            print(f"Sacado: {self.titular} de {valor:,.2f} reais na conta {self.id}")
+            self.__saldo -= valor
+            print(f"Sacado: {self._titular} de {valor:,.2f} reais na conta {self.id}")
 
 
 
