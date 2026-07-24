e34b31b8dffdb338f4a03aa4c6a6424777b683f4
Erasmo <erasmojunior456@gmail.com>
2026-07-24 19:01:36 -0300

    setter getter

--- exercicios/ex009/__main__.py
+++ exercicios/ex009/__main__.py
@@ -3,8 +3,10 @@ from rich import print, inspect
 
 
 def main():
-    av1 = Avaliacao("Pedro", "Matemática", 9.5)
-    inspect(av1)
+    av1 = Avaliacao("Pedro", "Matemática")
+    av1.set_nota(7.4)
+    print(f"{av1.nome} tirou {av1.get_nota()} em {av1.disciplina}")
+    #inspect(av1, private=True)
 
 if __name__ == "__main__":
     main()
--- exercicios/ex010/ex010.py
+++ exercicios/ex010/ex010.py
@@ -4,12 +4,16 @@ class Avaliacao:
         self.disciplina = disciplina
         self._nota = nota # Atributo Protected (#)
 
-    # Métodos Acessores
-    def get_nota(self): # Método Getter
+    # Criando Atributo Validável
+    @property
+    def nota(self): #getter
         return self._nota
 
-    def set_nota(self, valor): # Método Setter
+    @nota.setter
+    def nota(self, valor): #setter
         if 0 <= valor <= 10:
             self._nota = valor
         else:
             print("Nota invalida!")
+
+
