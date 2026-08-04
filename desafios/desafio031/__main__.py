9f105e5e467746fc44e5ec855962eed2ffbcd8db
Erasmo <erasmojunior456@gmail.com>
2026-08-04 18:32:56 -0300

    retangulo_properties

---
+++ desafios/desafio031/__main__.py
@@ -0,0 +1,16 @@
+from desafios.desafio031.classe031 import Retangulo
+
+def main():
+    r = Retangulo(7, 4)
+    try:
+        r.base = 12
+        r.altura = 7
+        r.medidas = (8, 12)
+    except Exception as e:
+        print(f"Ocorreu um erro do tipo {type(e).__name__} {e}")
+
+    print(r.medidas)
+
+
+if __name__ == "__main__":
+    main()
