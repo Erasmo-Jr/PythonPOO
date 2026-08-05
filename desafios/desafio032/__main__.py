2d2bd46566ed9b5e203ece4ad6867d278030683f
Erasmo <erasmojunior456@gmail.com>
2026-08-05 17:22:30 -0300

    desafio032

---
+++ desafios/desafio032/__main__.py
@@ -0,0 +1,7 @@
+from desafios.desafio032.classe032 import *
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
--- desafios/desafio032/classe032.py
+++ desafios/desafio032/classe032.py
@@ -7,11 +7,12 @@ class ContaBancaria:
     """
     Cria uma conta bancária e permite fazer saques e depósitos
     """
-    def __init__(self, id, nome, saldo = 0):
-        self.id = id # público (+)
+    def __init__(self, id:int, nome:str = None, saldo:float = 0, chave:str = None):
+        self._id = id # protegido (#)
         self._titular = nome # protegido (#)
         self.__saldo = saldo # privado (-)
-        print(f'A conta {self.id} de {self._titular} tem R${self.__saldo:,.2f} de __saldo')
+        self.__hash = chave.encode()
+        print(f'A conta {self._id} de {self._titular} tem R${self.__saldo:,.2f} de __saldo')
 
     def __str__(self):
         #return f'A conta {self._id} de {self._titular} tem R${self.__saldo:,.2f} de __saldo'
@@ -20,15 +21,15 @@ class ContaBancaria:
     def depositar(self, valor):
         valor = abs(valor)
         self.__saldo += valor
-        print(f"Deposito: {self._titular} de {valor:,.2f} reais na conta {self.id}")
+        print(f"Deposito: {self._titular} de {valor:,.2f} reais na conta {self._id}")
 
     def sacar(self, valor):
         valor = abs(valor)
         if valor >= self.__saldo:
-            print(f"Saque NEGADO de R$ {valor:,.2f} na conta {self.id}: SALDO INSUFICIENTE")
+            print(f"Saque NEGADO de R$ {valor:,.2f} na conta {self._id}: SALDO INSUFICIENTE")
         else:
             self.__saldo -= valor
-            print(f"Sacado: {self._titular} de {valor:,.2f} reais na conta {self.id}")
+            print(f"Sacado: {self._titular} de {valor:,.2f} reais na conta {self._id}")
 
 
 
